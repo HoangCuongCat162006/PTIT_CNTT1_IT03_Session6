c6efd546c4567f3cb7de7a5b44bf312822eed9b4
HoangCuongCat162006 <Cathoang162006@gmail.com>
2025-06-29 23:13:04 +0700

    Add files via upload

--- PTIT_CNTT1_IT03_Session6_Bai5.c
+++ PTIT_CNTT1_IT03_Session6_Bai5.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 
-int leoThang(int n) {
-    if (n==1) {
-        return 1;
+int findMax(int *arr,int size,int max) {
+    if (size ==0 ) {
+        return max;
     }
-    if (n==2) {
-        return 2;
+    if (arr[size -1]>max) {
+        max == arr[size -1];
     }
-    return leoThang(n-1) + leoThang(n-2);
+    return findMax(arr , size -1,max);
 }
 
+int findMin(int * arr,int size,int min) {
+    if (size ==0) {
+        return min;
+    }
+    if (arr[size-1]<min) {
+        min=arr[size-1];
+    }
+    return findMin(arr,size-1,min);
+}
 int main() {
     int n;
-    printf("Nhap so : ");
+    printf("Nhap so luong phan tu mang : ");
     scanf("%d",&n);
 
-    int c=leoThang(n);
-    printf("Cach la : %d",c);
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        printf("Moi ban nhap gia tri arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+    }
 
+
+int num1=arr[0];
+    int num2=arr[0];
+    int max= findMax(arr,n,num1);
+    int min=findMin(arr,n,num2);
+    printf("Max=%d \nMin=%d",max,min  );
 }
